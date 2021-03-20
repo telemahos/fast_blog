e45b0e045308ad8dd4dcbff4a6c59adc927d39f9
Konstantinos Kakoulis <kakoulis73@GMAIL.COM>
2021-03-20 18:05:52 +0200

    added readme.md

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/')
+def index():
+    return {'data': 'My Name is kostas'}
+
+@app.get('/about')
+def about():
+    return {'data': 'This is the about Page'}
+
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 source blog-env/bin/activate
+VS Theme: 'NoctisUva'
