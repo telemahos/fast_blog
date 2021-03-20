4ba249f7e52e8096ec1661671b251f6adb9f4f0b
Konstantinos Kakoulis <kakoulis73@GMAIL.COM>
2021-03-20 21:54:49 +0200

    create the project on his folder blog

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': 'My Name is kostas'}
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str]= None):
+    # http://127.0.0.1:8000/blog?limit=50&published=true
+    if published:
+        return {'data': f'Only {limit} PUBLISHED blogs can be showed'}
+    else:
+        return {'data': f'Only {limit} blogs can be showed'}
 
-@app.get('/about')
-def about():
-    return {'data': 'This is the about Page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'Unpublished Posts' }
 
+@app.get('/blog/{id}')
+def show_blog(id:int):
+    return {'data': id }
+
+@app.get('/blog/{id}/comments')
+def show_blog_comments(id:int):
+    return {'data': 'Some Blog comments'}
+
+# Create a Pydantic class for Blog posts 
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+# Create a post with the post method
+@app.post('/blog')
+def create_blog_post(blog: Blog):
+    return {'data': f'This is the Title {blog.title}'}
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5073)
