07c7000a8ab0d50b577afcc440bc1c68cc2180d2
Konstantinos Kakoulis <kakoulis73@GMAIL.COM>
2021-03-23 09:50:46 +0200

    CRUD in REST is ready!!! to roll

--- blog/database.py
+++ blog/database.py
@@ -6,8 +6,6 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException, Request
+from sqlalchemy.sql import text
 from sqlalchemy.orm import Session
 from . import schemas, models
 from .database import engine, SessionLocal
+import uvicorn
+# On video 2:04
 
 app = FastAPI()
 
@@ -14,10 +17,40 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
-def create(request: schemas.Blog, db : Session = Depends(get_db)):
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+async def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    query = text("""UPDATE blogs SET title=:title, body=:body WHERE id = :id""").params(title=request.title,body=request.body, id=id)
+    db.execute(query)
+    db.commit()
+    return request
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db : Session=Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail=f'The post with id {id} is not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return "deleted!"
+
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.post('/blog/{id}', status_code=200)
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'The Blog with ID {id} is not found')
+    return blog
